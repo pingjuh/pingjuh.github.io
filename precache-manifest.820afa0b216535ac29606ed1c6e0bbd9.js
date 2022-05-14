0a18f49dd459f5052eefe09c310d582b2ca0dc79
pingjuh <hpj@u.nus.edu>
2022-05-14 20:54:25 +0800

    Updates

---
+++ precache-manifest.820afa0b216535ac29606ed1c6e0bbd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42b753b2485d7479e88d68e3e13522f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4a01d7ee81008f7a0e2",
+    "url": "/static/css/main.712853d7.chunk.css"
+  },
+  {
+    "revision": "1544b3aaaec92774777d",
+    "url": "/static/js/2.0e997fd6.chunk.js"
+  },
+  {
+    "revision": "f4a01d7ee81008f7a0e2",
+    "url": "/static/js/main.2e1eaa0e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "70bc36c8b1b157a8fcd6083067f5ca25",
+    "url": "/static/media/education.70bc36c8.png"
+  }
+]);
