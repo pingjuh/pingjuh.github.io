84ced4c11c7895a5c29b4d190a722b121f59eb0f
pingjuh <hpj@u.nus.edu>
2022-10-27 23:49:52 +0800

    Updates

---
+++ precache-manifest.b224c44d36ac5ac3e39873b257a11a54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53d065321b7277ff46b9b254f6df2421",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46922ac174eb59f17306",
+    "url": "/static/css/main.3471fb86.chunk.css"
+  },
+  {
+    "revision": "09f31878467f876981a5",
+    "url": "/static/js/2.34f7ae21.chunk.js"
+  },
+  {
+    "revision": "46922ac174eb59f17306",
+    "url": "/static/js/main.123f52aa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4b4bf8fe87fb8a3c038aa050e72455cc",
+    "url": "/static/media/background.4b4bf8fe.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a5a502ab8113d39fca11783d59a18dc6.js"
+  "/precache-manifest.b224c44d36ac5ac3e39873b257a11a54.js"
 );
 
 self.addEventListener('message', (event) => {
