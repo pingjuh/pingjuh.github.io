c9531bd3767c45559b5ee531951d691d16d04ba5
pingjuh <45278408+pingjuh@users.noreply.github.com>
2022-10-27 22:12:37 +0800

    Update Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,7 +6,6 @@ const cards = [
   'https://upload.wikimedia.org/wikipedia/commons/f/f5/RWS_Tarot_08_Strength.jpg',
   'https://upload.wikimedia.org/wikipedia/commons/5/53/RWS_Tarot_16_Tower.jpg',
   'https://upload.wikimedia.org/wikipedia/commons/9/9b/RWS_Tarot_07_Chariot.jpg',
-  'https://upload.wikimedia.org/wikipedia/commons/8/8d/RWS-06-thelovers.jpg',
   'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/RWS_Tarot_02_High_Priestess.jpg/690px-RWS_Tarot_02_High_Priestess.jpg',
   'https://upload.wikimedia.org/wikipedia/commons/d/de/RWS_Tarot_01_Magician.jpg',
 ]
